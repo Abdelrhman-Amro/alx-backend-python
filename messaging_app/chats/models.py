dfbe3f2161842561180ac5fd68e7973de2ddc26b
“Abdelrhman-Amro” <abdamro288dev@gmail.com>
2024-12-27 21:56:43 +0200

    models completed

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -6,12 +6,7 @@ from django.db import models
 # Create your models here.
 
 
-# user_id (Primary Key, UUID, Indexed)
-# first_name (VARCHAR, NOT NULL)
-# last_name (VARCHAR, NOT NULL)
-# email (VARCHAR, UNIQUE, NOT NULL)
-# password_hash (VARCHAR, NOT NULL)
-# User Model that extends AbstractUser
+# user Model that extends AbstractUser
 class user(AbstractUser):
     class Roles(models.IntegerChoices):
         ADMIN = 1, "admin"
@@ -28,8 +23,8 @@ class user(AbstractUser):
         ]
 
 
-# Conversation Model
-class Conversation(models.Model):
+# conversation Model
+class conversation(models.Model):
     conversation_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True
     )
@@ -37,12 +32,12 @@ class Conversation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
-# Message Model
-class Message(models.Model):
+# message Model
+class message(models.Model):
     message_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True
     )
     sender = models.ForeignKey(user, on_delete=models.CASCADE)
-    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(conversation, on_delete=models.CASCADE)
     message_body = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)

3ab627562703c3120daa5fe66881a65650cbcef0
“Abdelrhman-Amro” <abdamro288dev@gmail.com>
2024-12-27 22:01:26 +0200

    Manage rest auth settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -41,6 +41,17 @@ INSTALLED_APPS = [
     "chats",
 ]
 
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",  # Require authentication by default
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",  # Use session-based authentication
+        "rest_framework.authentication.TokenAuthentication",  # Optional: token-based authentication
+    ],
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",

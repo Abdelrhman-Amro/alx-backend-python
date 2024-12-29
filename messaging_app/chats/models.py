54ebc2e6f82e2e6ec3d49eef6fa503ad0e80fea9
“Abdelrhman-Amro” <abdamro288dev@gmail.com>
2024-12-29 20:19:34 +0200

    fix models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -7,15 +7,12 @@ from django.db import models
 
 
 # user Model that extends AbstractUser
-class user(AbstractUser):
+class User(AbstractUser):
     class Roles(models.IntegerChoices):
         ADMIN = 1, "admin"
         HOST = 2, "host"
         GUEST = 3, "guest"
 
-    user_id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True
-    )
     phone_number = models.CharField(max_length=50, null=True)
     role = models.IntegerField(choices=Roles.choices)
 
@@ -25,7 +22,7 @@ class conversation(models.Model):
     conversation_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True
     )
-    participants = models.ManyToManyField(user)
+    participants = models.ManyToManyField(User)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
@@ -34,7 +31,7 @@ class message(models.Model):
     message_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True
     )
-    sender = models.ForeignKey(user, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
     conversation = models.ForeignKey(conversation, on_delete=models.CASCADE)
     message_body = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)

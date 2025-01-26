cae725bb40d1756d9edf6c6cde8ebe2ec85b17d0
“Abdelrhman-Amro” <abdamro288dev@gmail.com>
2025-01-26 13:33:12 +0200

    Fix models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -9,6 +9,8 @@ class User(AbstractUser):
     USER_ROLES = [("guest", "Guest"), ("host", "Host"), ("admin", "Admin")]
 
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
     password_hash = models.CharField(max_length=255, null=False)
     phone_number = models.CharField(max_length=20, null=True)
     role = models.CharField(
